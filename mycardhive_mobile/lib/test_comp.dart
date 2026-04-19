69b42856e502d52a90a3cec8f0eb7675716eb4a9
Abdul <abdul@example.com>
2026-04-19 13:18:35 +0100

    Cleanup: Remove test file

--- mycardhive_mobile/lib/test_comp.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-void main() {
-  print('Checking FlutterLocalNotificationsPlugin.initialize signature...');
-  // This is a dummy script just to see if it compiles
-  final plugin = FlutterLocalNotificationsPlugin();
-  // We will try different ways to call it
-}
